f78f86060340d2faea5b8e08f4ebd7f1d26af44b
Caden Wilcox <wilcox4357@gmail.com>
2021-09-28 17:00:45 -0700

    getting wireframe ready for storyline project

---
+++ Week1/Algos/AlgoPractice1.py
@@ -0,0 +1,65 @@
+# Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.
+def swap():
+    myNumber = 42
+    myName = "Caden"
+    temp = myNumber
+    myNumber = myName
+    myName = temp
+    print(f'My name is {myName}')
+    return(f'My number is {myNumber}')
+print(swap())
+
+# Print integers from -52 to 1066 using a FOR loop.
+
+def printNumbers():
+    for i in range(-52, 1067, 1):
+        print(i)
+printNumbers()
+
+# Create beCheerful(). Within it, console.log string "good morning!" Call it 98 times.
+def beCheerful():
+    print('Good Morning ' * 98)
+beCheerful()
+
+# Using FOR, print multiples of 3 from -300 to 0. Skip -3 and -6.
+def multiplesOfThree():
+    for i in range(-300, 1, 1):
+        if(i == -3 or i == -6):
+            continue
+        else:
+            print(i)
+multiplesOfThree()
+
+# Print integers from 2000 to 5280, using a WHILE.
+def printWithWhileLoop():
+    i = 2000
+    while i <= 5280:
+        print (i)
+        i += 1
+printWithWhileLoop()
+
+# If 2 given numbers represent your birth month and day in either order, log "How did you know?", else log "Just another day...." 
+def birthday(num1, num2):
+    birthdayNum1 = 9
+    birthdayNum2 = 19
+    if(num1 == birthdayNum1 or num1 == birthdayNum2 & num2 == birthdayNum2 or num2 == birthdayNum1):
+        print("Happy birthday")
+    else:
+        print("Just another day")
+
+birthday(6,10)
+birthday(9,19)
+
+#Write a function that determines whether a given year is a leap year. If a year is divisible by four, it is a leap year, unless it is divisible by 100. However, if it is divisible by 400, then it is.
+
+
+# Print all integer multiples of 5, from 512 to 4096. Afterward, also log how many there were.
+
+def multiplesOfFive():
+    count = 0
+    for i in range(512, 4097, 1):
+        if(i % 5 == 0):
+            count += 1
+            print(i)
+    return count
+print(multiplesOfFive())
