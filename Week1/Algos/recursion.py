2de022e21a292371e246beb710c400bbd07b8665
Caden Wilcox <wilcox4357@gmail.com>
2021-11-09 13:10:06 -0800

    working on recursion and reusable css grid

--- Week1/Algos/recursion.py
+++ Week1/Algos/recursion.py
@@ -30,4 +30,24 @@ end = perf_counter()
 print(end-start)
 
 
+# fibinocci iterative function that returns an array of fib numbers up to the number you put in 
+def fibs(n):
+    f = []
+    a = 0
+    b = 1
+    if n == 0 or n == 1:
+        print (n)
+    else:
+        f.append(a)
+        f.append(b)
+        while len(f) != n:
+            temp = a + b
+            f.append(temp)
+            a = b
+            b = temp
+    print (f)
+fibs(10)
+
+def fib(num):
+    
 
