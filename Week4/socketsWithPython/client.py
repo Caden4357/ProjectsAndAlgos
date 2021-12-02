d99cf7f13b205fac2034ea40483b7d9c17310d84
Caden Wilcox <wilcox4357@gmail.com>
2021-12-01 22:29:06 -0800

    more on sockets with py sending objects w/pickle

--- Week4/socketsWithPython/client.py
+++ Week4/socketsWithPython/client.py
@@ -1,11 +1,27 @@
 import socket
+import pickle
+
+HEADERSIZE =  10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
 
 while True:
-    msg = s.recv(8)
-    print(msg.decode("utf-8"))
+    full_msg = b""
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message length: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+        full_msg += msg
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg received")
+            print(full_msg[HEADERSIZE:])
+            dict = pickle.loads(full_msg[HEADERSIZE:])
+            print(dict)
+            new_msg = True
+            full_msg = b""
+        print(full_msg)
--- Week4/socketsWithPython/server.py
+++ Week4/socketsWithPython/server.py
@@ -1,9 +1,34 @@
 import socket
+import time
+# pickle allows us to convert objects to bytes so we can send and receive them 
+import pickle
+
+
+HEADERSIZE =  10
+
+msg = "Welcome to the server!"
+print(f"{len(msg):<10}")
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientsocket.close()
+
+    dict = {1: "Caden", 2: "Wilcox"}
+    msg = pickle.dumps(dict)
+
+    msg = bytes(f"{len(msg):<{HEADERSIZE}}", "utf-8") + msg
+
+    clientsocket.send(bytes(msg))
+
+
+
+# Cool way to send the time to the client after every 3 seconds that pass 
+    # while True:
+    #     time.sleep(3)
+    #     msg = f"the time is: {time.time()}"
+    #     msg = f"{len(msg):<{HEADERSIZE}}" + msg
+    #     clientsocket.send(bytes(msg, "utf-8"))
